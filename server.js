de23534596d97f538591208ab03013c6461b7902
DiyorBaynazarov <diyorbaynazarov@pop-os.localdomain>
2022-07-12 18:19:35 +0500

    connecting to remote db

--- server.js
+++ server.js
@@ -26,10 +26,8 @@ const app = express();
 const postgres = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'diyorbaynazarov',
-    password: 'Universe2000@',
-    database: 'project',
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
   },
 });
 
