2d56dfa7c785e9a71d0cf2d8182fd6ee37a922e6
DiyorBaynazarov <diyorbaynazarov@pop-os.localdomain>
2022-07-12 18:46:33 +0500

    ssl config new

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ const postgres = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
